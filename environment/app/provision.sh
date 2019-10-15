ebbf98f77de22d56d9d2476f835cd08958843e6e
Jillian Guillen <JGuillen@spartaglobal.com>
2019-10-15 17:05:28 +0100

    initialising app project

---
+++ environment/app/provision.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install pm2 -g
+
+# Install gems
+sudo gem install bundler
+sudo gem install rake
+sudo gem install rails
+
+# set up nginx reverse proxy
+sudo ln -s /home/ubuntu/environment/nodeapp.conf /etc/nginx/conf.d/nodeapp.conf
+sudo nginx -s reload
